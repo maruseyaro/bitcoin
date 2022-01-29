fee86d55cfc3b833eeb9c3f119f684c526e58dc8
Marcelo Alves <maruseyaro@gmail.com>
2022-01-29 13:56:14 +0900

    Add bitcoin api calls.

--- src/index.tsx
+++ src/index.tsx
@@ -62,6 +62,37 @@ async function fetchLatestPosts() {
   return posts;
 }
 
+// {"price":29546,"stamp":1643430240}
+async function fetchBitcoinCurrentPrice() {
+  const url = "https://index-api.bitcoin.com/api/v0/cash/price/usd";
+  const res = await fetch(url);
+  const json = await res.json();
+
+  console.log(json);
+
+  return json;
+}
+
+/*
+
+[
+    [
+        "2017-01-01T00:00:00.000Z",
+        96760
+    ],
+*/
+
+// https://github.com/bitcoin-portal/index-api-docs#historic-index-values
+async function fetchBitcoinHistory() {
+  const url = "https://index-api.bitcoin.com/api/v0/history";
+  const res = await fetch(url);
+  const json = await res.json();
+
+  console.log(json);
+
+  return json;
+}
+
 function App() {
   return (
     <>
